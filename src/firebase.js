b163f2c7030f38751bfb53d99497f6cd8e1e9d2d
Biljeg <biljeg8@outlook.com>
2022-06-01 22:52:45 +0200

    inited cra, firebase and installed some extentions

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDLL7zlXMRTo0Q7uHtB01p5hwEAuRyiyjw",
+
+	authDomain: "yelpcamp-7f123.firebaseapp.com",
+
+	projectId: "yelpcamp-7f123",
+
+	storageBucket: "yelpcamp-7f123.appspot.com",
+
+	messagingSenderId: "159127553491",
+
+	appId: "1:159127553491:web:50d26b283f6889ad07db26",
+}
+
+const app = initializeApp(firebaseConfig)
